1148fdd9b3cdbf75082711bdf14bf37c64770439
fuzhong <likemiddle@gmail.com>
2019-06-06 17:33:55 +0800

    init

---
+++ config/dev.env.js
@@ -0,0 +1,6 @@
+module.exports = {
+    NODE_ENV: '"development"',
+    // BASE_API: '"http://47.107.178.240:9002"',
+    BASE_API: '"http://47.96.85.51:9002"',
+    APP_ORIGIN: '"https://wallstreetcn.com"'
+}
---
+++ src/views/monitor/eureka/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <iframe src='http://47.96.85.51:9002/druid' id='show' width='100%' height="780px" frameborder=0 name='showHere' ></iframe>
+</template>
